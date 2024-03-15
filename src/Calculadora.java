ed0830d92c3dce6851d1d77e754f8bf4d2c5c4c0
Paula <paulavca46@educastur.es>
2024-03-15 13:20:58 +0100

    Versión 0

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
 
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int num1;
+		
+		int num2;
+		
+		System.out.println("Introduce el primer número");
+		
+		num1 = entrada.nextInt();
+		
+		System.out.println("Introduce el segundo número");
+		
+		num2 = entrada.nextInt();
+		
+		entrada.close();
+		
+		
+		int suma = num1 + num2;
+		
+		int resta = num1 - num2;
+		
+		int multiplicacion = num1 * num2;
+	
+		int division = num1 / num2;
+		
+
+		System.out.println(num1 + " + " + num2 + " = " + suma);
+		
+		System.out.println(num1 + " - " + num2 + " = " + resta);
+		
+		System.out.println(num1 + " * " + num2 + " = " + multiplicacion);
+		
+		System.out.println(num1 + " / " + num2 + " = " + division);
 
 	}
 
